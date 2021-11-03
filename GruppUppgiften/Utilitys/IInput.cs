4e455118b93703b531098e697d2a72e7afc65867
saldermorth <77542775+saldermorth@users.noreply.github.com>
2021-11-03 05:56:49 +0100

    Util written

---
+++ GruppUppgiften/Utilitys/IInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GruppUppgiften.Utilitys
+{
+    interface IInput
+    {
+        int GetInt();
+        string GetString();
+
+       // List<Vehicle> GetListOfVehicle();
+    }
+}
---
+++ GruppUppgiften/Utilitys/InputService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GruppUppgiften.Utilitys
+{
+    class InputService : IInput
+    {
+        public int GetInt() //No in paramater
+        {
+            int integer;
+            while (!int.TryParse(Console.ReadLine(), out integer))
+            {
+                Console.WriteLine("Input of type integer needed");
+                Console.Write("Try Again : ");
+            }
+            return integer;
+        }
+
+        public string GetString()
+        {
+            string str = Console.ReadLine();
+            while (str.Any(char.IsDigit)  || string.IsNullOrEmpty(str))
+            {
+                Console.WriteLine("Input of type string needed");
+                Console.Write("Try Again : ");
+            }
+            return str;
+        }
+    }
+}
