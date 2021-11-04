c0ffacd3e0bdd99e1071a4c16777bd2512d79550
Anton Edholm <50101559+Draggel12345@users.noreply.github.com>
2021-11-04 14:34:06 +0100

    Added service

---
+++ GruppUppgiften/Service/GarageImpl.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GruppUppgiften.Service
+{
+    class GarageImpl : IGarage<T>
+    {
+        //private readonly IVehicel<T> dao = new VehicelImpl();
+        public T AddVehicle(T obj)
+        {
+            //return dao.AddVehicles(obj);
+            throw new NotImplementedException();
+        }
+
+        public List<T> ListTypeOfVehicles(T obj)
+        {
+            //List<T> temp = dao.ListTypeOfVehicle(obj.Type);
+            //if(!temp.Any())
+            //{
+            // Console.WriteLine($"There are no Vehicles with type: {obj.Type}");
+            //}
+            //return temp;
+            throw new NotImplementedException();
+        }
+
+        public List<T> ListVehicles()
+        {
+            //List<T> temp = dao.ListVehicle();
+            //if(!temp.Any())
+            //{
+            // Console.WriteLine($"The list is empty.");
+            //}
+            //return temp;
+            throw new NotImplementedException();
+        }
+
+        public void RemoveVehicle(int id)
+        {
+            //T toRemove = dao.FindById(id);
+            throw new NotImplementedException();
+        }
+
+        public T SearchVehicle(T obj)
+        {
+            //T toFind = dao.SearchVehicle(obj.Id, obj.Type);
+            //if(toFind == null)
+            //{
+            //    Console.WriteLine($"Could not be found in the list.");
+            //}
+            //return toFind;
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GruppUppgiften/Service/IGarage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GruppUppgiften.Service
+{
+    interface IGarage<T>
+    {
+        List<T> ListVehicles();
+        List<T> ListTypeOfVehicles(T obj);
+        T AddVehicle(T obj);
+        void RemoveVehicle(int id);
+        T FindById(int id);
+        T SearchVehicle(T obj);
+    }
+}
