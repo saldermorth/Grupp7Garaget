8d3aa1edf2d13c416843d7e66b2759ebb8ea39c5
Leven Darwich <LevenDarwich@hotmail.se>
2021-11-05 13:27:36 +0100

    ny commit

---
+++ GruppUppgiften/Moped.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GruppUppgiften
+{
+    class Moped : Vehicle
+    {
+        public bool IsPedal { get; set; }
+        public bool HasHorn { get; set; }
+        public Moped(int amountofwheeles, string color, string reg_nr, string type, string model, string tillverkare, int Id, bool ispedal, bool hashorn) : base (amountofwheeles, color, reg_nr, type, model, tillverkare, Id)
+        {
+            ispedal = IsPedal;
+            hashorn = HasHorn;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}. Type: {Type}. Model: {Model}. Tillverkare: {Tillverkare}. " +
+                   $"Color: {Color}. Antal hjul: {AmountOfWheeles}. Regnummer: {Reg_Nr}." +
+                   $"Har tuta: {HasHorn}. Ispedal: {IsPedal}"; // Ändra ispedal
+        }
+    }
+}
