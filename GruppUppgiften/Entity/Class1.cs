2fc8ea281fa52ec508fdff1c3f4042379fa00a1b
emre ersoylu <emreersoylu@hotmail.com>
2021-11-05 14:16:01 +0100

    Motorcycle

---
+++ GruppUppgiften/Entity/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Motorcycle
+{
+    public class Motorcycle : Vehical
+    {
+        public bool IsOffroad { get; set; }
+        public int Weight { get; set; }
+        public Motorcycle(int Id, string type, string Model, string Tillverkare, string Color, int AmountOfWheels, int Reg_Nr, bool IsOffroad, int Weight) : base (amountOfWheels, color, reg_Nr, type, model, tillverkare, id)
+        {
+            IsOffroad = IsOffroad;
+            Weight = Weight;
+        }
+        public override string ToString()
+        {
+            return $"Id: {Id}. Type: {type}. Model: {Model}. Tillverkare: {Tillverkare}. " +
+                   $"Color: {Color}. Antal hjul: {AmountOfWheeles}. Regnummer: {Reg_Nr}." +
+                   $"Is Offroad: {IsOffroad}. Weight: {Weight}";
+        }
+    }
+}
